4bc7271335cf5514381c4310c6069be1eb977a53
Yash <132873361+yashuOP16@users.noreply.github.com>
2026-03-30 17:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	let count = {};
+
+	for(let i = 0 ; i < str.length ; i++){
+		let ch = str[i];
+		const[ch] = (count [ch] || 0) + 1;
+	}
+
+	for(let i = 0; i < str.length; i++){
+		if(count [str[i]] === 1){
+			return str[i];
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
